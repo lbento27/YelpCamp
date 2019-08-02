6efb1a7339e1d0bf9b336700798653fcf8bcc0ac
Bento <lbento525@gmail.com>
2019-08-02 17:33:04 +0100

    add route and page to new password(reset), and link to forgot(login page)

--- routes/user.js
+++ routes/user.js
@@ -56,7 +56,7 @@ router.post("/forgot", function(req, res, next) {
       function(done) {
         crypto.randomBytes(20, function(err, buf) {
           var token = buf.toString("hex");
-          done(err, token);
+          done(err, token); //(done)=move on to the next function
         });
       },
       //find the email and if exists
@@ -118,6 +118,100 @@ router.post("/forgot", function(req, res, next) {
     function(err) {
       if (err) return next(err);
       res.redirect("/forgot");
+    }
+  );
+});
+//!===========================
+//? show form to reset password
+router.get("/reset/:token", function(req, res) {
+  //find user token send to email and check if time(password reset on db) is greater than now
+  User.findOne(
+    {
+      resetPasswordToken: req.params.token,
+      resetPasswordExpires: { $gt: Date.now() }
+    },
+    function(err, user) {
+      if (!user) {
+        req.flash("error", "Password reset token is invalid or has expired.");
+        return res.redirect("/forgot");
+      }
+      //render reset form and pass token
+      res.render("users/reset", { token: req.params.token });
+    }
+  );
+});
+
+//? handle password reset (new password)
+
+router.post("/reset/:token", function(req, res) {
+  //async.water is an array of functions that will be auto call one after another
+  async.waterfall(
+    [
+      function(done) {
+        //find user by token and check date of token DB if its greater than now
+        User.findOne(
+          {
+            resetPasswordToken: req.params.token,
+            resetPasswordExpires: { $gt: Date.now() }
+          },
+          function(err, user) {
+            if (!user) {
+              req.flash(
+                "error",
+                "Password reset token is invalid or has expired."
+              );
+              return res.redirect("back");
+            }
+            //if we get user back
+            //check if password on the two form match
+            if (req.body.password === req.body.confirm) {
+              //.setPassword its from passport local mongoose that will do all the hash and encrypt auto and we only nedd to pass the password
+              user.setPassword(req.body.password, function(err) {
+                //reset token
+                user.resetPasswordToken = undefined;
+                user.resetPasswordExpires = undefined;
+                //save/update user and login
+                user.save(function(err) {
+                  req.logIn(user, function(err) {
+                    done(err, user); //(done)=move on to the next function
+                  });
+                });
+              });
+            } else {
+              req.flash("error", "Passwords do not match.");
+              return res.redirect("back");
+            }
+          }
+        );
+      },
+      //send email to confirmation password reset
+      //NOTE: in order to send mail with Gmail account need to enable "access to application less secure" for that account, link=https://myaccount.google.com/lesssecureapps?pli=1
+      function(user, done) {
+        var smtpTransport = nodemailer.createTransport({
+          service: "Gmail",
+          auth: {
+            user: "yelpcamppt@gmail.com",
+            pass: process.env.GMAILPW //password to ENV set up locally($ export GMAILPW=jdfbjdg) and on heroku
+          }
+        });
+        var mailOptions = {
+          to: user.email,
+          from: "yelpcamppt@mail.com",
+          subject: "Your password has been changed",
+          text:
+            "Hello,\n\n" +
+            "This is a confirmation that the password for your account " +
+            user.email +
+            " has just been changed.\n"
+        };
+        smtpTransport.sendMail(mailOptions, function(err) {
+          req.flash("success", "Success! Your password has been changed.");
+          done(err);
+        });
+      }
+    ],
+    function(err) {
+      res.redirect("/campgrounds");
     }
   );
 });
